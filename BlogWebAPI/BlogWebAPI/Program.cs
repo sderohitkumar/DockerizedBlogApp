0b7f91c33b499b744b629e88dbb5206dc26234e4
Rohit <rohit.kumar09@nagarro.com>
2025-03-30 18:25:04 +0530

    docker and docker compose updated to include env variables.

--- BlogWebAPI/BlogWebAPI/Program.cs
+++ BlogWebAPI/BlogWebAPI/Program.cs
@@ -2,6 +2,8 @@ using BlogWebAPI.Context;
 using BlogWebAPI.Extensions;
 using BlogWebAPI.Services;
 
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "production";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile(
+        $"appsettings.{environment}.json",
+        optional: false,
+        reloadOnChange: true
+     ).AddEnvironmentVariables();
+
 var sqlConnectionString = builder.Configuration.GetConnectionString("BlogDatabase");
 var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
 builder.Services.AddSqlServer<BlogDbContext>(sqlConnectionString);
